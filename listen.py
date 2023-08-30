f2ff9bafaf4c193905903f86f0dba5009c679d8f
Benjamin Wang <w.benji@tuta.io>
2023-08-30 21:03:34 +1000

    update

---
+++ listen.py
@@ -0,0 +1,40 @@
+import paho.mqtt.client as mqtt
+import serial
+
+ 
+
+def on_connect(client, userdata, flags, rc): # func for makinf connection
+    print(f"Connected to MQTT Server {ADD} at port {PORT}")
+    print(f"Connection returned result: " + str(rc) )
+    topic = "bengy"
+    client.subscribe(topic)
+    print(f"Subscribe to topic: {topic}")
+
+
+ 
+
+def on_message(client, userdata, msg): #Func for Sending msg
+    print("recieved message!")
+    print(msg.topic+" "+str(msg.payload))
+    print("relying to teensy over BT...")
+
+    #Code from last week's bt.py
+    #relay the command from Pi to Teensy
+    #Connect Pi with Teensy over BT first
+    ser = serial.Serial("/dev/rfcomm0", 9600)
+    ser.write(msg.payload)
+
+ADD = "3.25.58.119"
+PORT = 1883
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+ 
+
+client.connect(ADD,PORT, 60)
+
+ 
+
+client.loop_forever()
